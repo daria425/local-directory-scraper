7798b0034fac46f4c6a2420a4e9e697149e8ed0f
daria425 <darianaumova5@gmail.com>
2024-06-28 02:30:07 +0300

    send file clientside

--- server/src/routes/csv_file.py
+++ server/src/routes/csv_file.py
@@ -1,11 +1,16 @@
 from fastapi import APIRouter, Query
+from fastapi.responses import Response
 from ..dependencies.make_csv import make_category_csv
 from ..models.ScrapeRequest import ScrapeRequest
 from ..dependencies.helpers import replace_url
+
+
 router=APIRouter(prefix="/csv-file")
 
 @router.post("/")
 def send_csv(request_url: ScrapeRequest, region: str = Query(..., description="Region of the URL")):
     url=replace_url(request_url.url)
-    df_json=make_category_csv(url, region)
-    return df_json
+    df=make_category_csv(url, region)
+    response_headers={'Content-Disposition': 'attachment; filename="data.csv"'}
+    return Response(df.to_csv(index=False), headers=response_headers, media_type="text/csv" )
+    
