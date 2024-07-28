aca1460be2b2d737a767036c34cb0a6e0db17e8e
daria425 <darianaumova5@gmail.com>
2024-07-28 12:10:31 +0300

    remove client directory

--- client/src/Loader.jsx
+++
@@ -1,8 +0,0 @@
-export default function Loader() {
-  return (
-    <div className="loader-container">
-      <p className="loader-container__caption">Loading...</p>
-      <div className="loader"></div>
-    </div>
-  );
-}
--- client/src/api/api_base.js
+++
@@ -1,6 +0,0 @@
-let api_base = import.meta.env.VITE_DEV_API_BASE;
-if (import.meta.env.PROD) {
-  api_base = import.meta.env.VITE_PROD_API_BASE;
-}
-
-export { api_base };
